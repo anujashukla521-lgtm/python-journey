93bfbc8af34bdbb1e71d4e24f60753e256fe3d05
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-03 12:07:24 +0530

    Code 1

---
+++ Basics/basic_calculator.py
@@ -0,0 +1,11 @@
+# This Python program accepts two user inputs and demonstrates fundamental arithmetic operations, helping build a strong foundation in variables, input handling, and operators.
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print("Addition of",x,"and",y,"is:",x+y)
+print("Subtraction of",x,"and",y,"is:",x-y)
+print("Multiplication of",x,"and",y,"is:",x*y)
+print("Division of",x,"and",y,"is:",x/y)
+print("Modulus of",x,"and",y,"is:",x%y)
+print("Floor division of",x,"and",y,"is:",x//y)
+print("Exponent of",x,"and",y,"is:",x**y)
