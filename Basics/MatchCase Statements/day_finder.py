f9626dc1de9734fd17d2e379ba03406e71c895df
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-08 12:15:52 +0530

    Code 1

---
+++ Basics/MatchCase Statements/day_finder.py
@@ -0,0 +1,33 @@
+# Finds the day of the week using a number and match-case statements.
+
+num = int(input("Enter a num between 1-7: "))
+match num: 
+    case 1:
+        print("Monday!")
+    case 2:
+        print("Tuesday!")
+    case 3:
+        print("Wednesday!")
+    case 4:
+        print("Thursday!")
+    case 5:
+        print("Friday!")
+    case 6:
+        print("Saturday!")
+    case 7:
+        print("Sunday!")
+    case _:
+        print("Invalid day")
+
+
+
+
+
+
+
+
+
+
+
+
+
