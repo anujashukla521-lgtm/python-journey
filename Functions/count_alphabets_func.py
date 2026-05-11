0f553afdc7da6170dd32f7f8b5b86177744ea106
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-11 12:11:11 +0530

    Code 6

--- Functions/count_alphabets_func.py
+++ Functions/count_alphabets_func.py
@@ -1,12 +1,14 @@
+# A function-based program that counts vowels and consonants in a string using loops and conditional logic.
+
 def count(st):
     vowel_count = 0
     consonant_count = 0
     for i in st:
         if i.isalpha():
             if i in "aeiouAEIOU":
-                vowel_count = vowel_count+1
+                vowel_count +=1
             else:
-                consonant_count = consonant_count+1
+                consonant_count +=1
     return vowel_count, consonant_count
 
 
