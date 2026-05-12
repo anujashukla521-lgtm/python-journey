e821cec1066ed93027d39637f7c93761c3796c3c
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-12 12:22:04 +0530

    Code 4

--- Lists/manual_sort.py
+++ Lists/manual_sort.py
@@ -1,3 +1,5 @@
+# Sorts list elements in ascending order manually without using the built-in sort() method.
+
 nums = [4,8,2,7,11,1,6]
 for i in range(len(nums)):
     for j in range(i+1,len(nums)):
