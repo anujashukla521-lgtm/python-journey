df31b26863dfec7b11a64c2deb1d2c4e3c4540c4
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-16 18:27:53 +0530

    Code 1

--- Basics/fstrings_docstrings/student_intro.py
+++ Basics/fstrings_docstrings/student_intro.py
@@ -1,3 +1,5 @@
+# A Python program that takes user details like name, age, and course, then displays a formatted introduction using f-strings.
+
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 course = input("Enter course you are pursuing: ")
