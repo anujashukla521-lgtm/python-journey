5ecf28b0b814a0f33e59b3e1b2252525752043b7
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-16 18:29:25 +0530

    Code 5

--- Basics/fstrings_docstrings/greeting_function_docstring.py
+++ Basics/fstrings_docstrings/greeting_function_docstring.py
@@ -1,3 +1,5 @@
+# A Python function that greets the user with a personalized welcome message using f-strings and docstrings.
+
 def greeting(n):
     '''Greets the user with welcome message'''
     print(f"Hey, {n} welcome!")
