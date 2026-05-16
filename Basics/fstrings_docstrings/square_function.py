d6d46feef9e304c281459eb3b16ecf5e99014520
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-16 18:28:58 +0530

    Code 4

--- Basics/fstrings_docstrings/square_function.py
+++ Basics/fstrings_docstrings/square_function.py
@@ -1,3 +1,5 @@
+# A function-based program that returns the square of a number and demonstrates the use of docstrings in Python.
+
 def square(n):
     '''Takes a number from user and returns its square'''
     return n**2
