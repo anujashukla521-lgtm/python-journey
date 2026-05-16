16a1481c69890404f6b87ec6912edba6620d55c7
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-16 18:28:15 +0530

    Code 2

--- Basics/fstrings_docstrings/marks_summary.py
+++ Basics/fstrings_docstrings/marks_summary.py
@@ -1,3 +1,5 @@
+# A program that accepts marks of three subjects, calculates total and average marks, and displays the result using formatted output.
+
 marks1 = float(input("Enter marks of subject 1: "))
 marks2 = float(input("Enter marks of subject 2: "))
 marks3 = float(input("Enter marks of subject 3: "))
