0b17bee0e61d61b6c11d6dc727526627fcdf0082
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-20 17:54:16 +0530

    Code 8

--- Sets/common_subjects.py
+++ Sets/common_subjects.py
@@ -1,3 +1,5 @@
+# Finds common subjects liked by two students using set intersection.
+
 student1 = {"Math", "Python", "Physics"}
 student2 = {"Python", "English", "Math"}
 print(student1.intersection(student2))
