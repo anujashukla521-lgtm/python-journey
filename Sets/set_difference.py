7d1247e18ae5f2af14007d13362f72bf70592c2e
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-20 17:53:19 +0530

    Code 6

--- Sets/set_difference.py
+++ Sets/set_difference.py
@@ -1,3 +1,5 @@
+# Prints elements that are present in the first set but not in the second set.
+
 a = {"korea","kabul","china","america","italy"}
 b = {"madrid","korea","berlin","scotland","china"}
 print(a.difference(b))
