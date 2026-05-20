215cebf8a97cbce903170d58995dd7d47223fd09
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-20 17:53:46 +0530

    Code 7

--- Sets/symmetric_diff.py
+++ Sets/symmetric_diff.py
@@ -1,3 +1,5 @@
+# Displays elements that are unique to each set by removing common elements.
+
 a ={"apple", "banana","watemelon","guava","kiwi"}
 b = {"mango","lichi","strawberry","apple","kiwi"}
 print(a.symmetric_difference(b))
