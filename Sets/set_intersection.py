6e2d7b7122da3600fc6f05eb6eb3f6422cdf97bd
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-20 17:52:40 +0530

    Code 5

--- Sets/set_intersection.py
+++ Sets/set_intersection.py
@@ -1,3 +1,5 @@
+# Finds and displays common elements between two sets.
+
 a = {"korea","kabul","china","america","italy"}
 b = {"madrid","korea","berlin","scotland","china"}
 print(a.intersection(b))
