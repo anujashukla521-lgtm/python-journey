9a62e34670c089bd50aedb5ef5dccfde9260ee7f
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-22 20:32:08 +0530

    Code 2

--- Basics/Loops/loop_else/login_attempts.py
+++ Basics/Loops/loop_else/login_attempts.py
@@ -1,3 +1,5 @@
+# A Python program that allows a user up to three password attempts using a while loop with else, displaying login success or account lock messages.
+
 password = "python123"
 attempts = 0
 while attempts<3:
