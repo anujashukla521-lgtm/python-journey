1c83f4902509e0074f637570f2c99c233d213e40
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-22 20:31:39 +0530

    Code 1

--- Basics/Loops/loop_else/search_number.py
+++ Basics/Loops/loop_else/search_number.py
@@ -1,3 +1,5 @@
+# A Python program that searches for a user-entered number in a list using a for loop with else, printing whether the number was found or not.
+
 lst = [6,8,5,9,2,3]
 num = int(input("Enter num to search: "))
 for i in lst:
