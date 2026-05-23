f01f906f50856dca2388b2d928272a3ed1845bd9
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-23 15:56:08 +0530

    Code 3

--- Exception Handling/dictionary_lookup.py
+++ Exception Handling/dictionary_lookup.py
@@ -1,3 +1,5 @@
+# Looks up a student’s marks from a dictionary based on user input and handles the case where the entered key does not exist.
+
 dic = {"Sam": 67, "Rohan": 98, "John": 76, "Mohit": 85}
 try:
     name = input("Enter student name: ")
