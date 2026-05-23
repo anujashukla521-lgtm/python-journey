affc6387e318ffdf0cdc38aab9437bb8f6e4368e
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-23 15:55:37 +0530

    Code 2

--- Exception Handling/safe_list_access.py
+++ Exception Handling/safe_list_access.py
@@ -1,3 +1,5 @@
+# Allows the user to access an element from a list using an index and safely handles invalid inputs and out-of-range index errors.
+
 lst = ["Harry","Ron","Hermoine","Luna","Draco"]
 
 try:
