63a1c1751f5396ed39e5a7104049d72346e91672
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-23 15:55:11 +0530

    Code 1

--- Exception Handling/safe_division.py
+++ Exception Handling/safe_division.py
@@ -1,3 +1,5 @@
+# Takes two numbers from the user and performs division while handling errors like invalid input and division by zero using exception handling.
+
 try:
     num1 = int(input("Enter first number: "))
     num2 = int(input("Enter second number: "))
