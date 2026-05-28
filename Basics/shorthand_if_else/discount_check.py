055bd02c3908ad88a78cb9895d49ee712f89b56b
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-28 17:20:54 +0530

    Code 9

--- Basics/shorthand_if_else/discount_check.py
+++ Basics/shorthand_if_else/discount_check.py
@@ -1,3 +1,5 @@
+# Calculates discount eligibility based on purchase amount.
+
 discount = 200
 purchase = int(input("Enter purchase amount: "))
 print("Discount price", purchase-discount) if purchase>1000 else print("Purchase price", purchase)
