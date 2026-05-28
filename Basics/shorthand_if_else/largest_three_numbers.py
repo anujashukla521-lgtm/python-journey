c8c1c52eb84e3145c19c85b752f15d832d72e7e0
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-28 17:21:31 +0530

    Code 10

--- Basics/shorthand_if_else/largest_three_numbers.py
+++ Basics/shorthand_if_else/largest_three_numbers.py
@@ -1,4 +1,6 @@
+# Finds the largest number among three inputs using nested shorthand if-else.
+
 num1= int(input("Enter first number: "))
 num2= int(input("Enter second number: "))
 num3= int(input("Enter third number: "))
-print(f"{num1} is greater") if num1>num2 and num1>num3 else print(f"{num2} is greater") if num2>num1 and num2>num3 else print(f"{num3} is greater")
+print(f"{num1} is greatest") if num1>num2 and num1>num3 else print(f"{num2} is greatest") if num2>num1 and num2>num3 else print(f"{num3} is greatest")
