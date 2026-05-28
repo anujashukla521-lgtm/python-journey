3b11e0815d8516ce278196f4c53b4809bf8521b3
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-28 17:57:02 +0530

    Code 5

--- Enumerate/shopping_list.py
+++ Enumerate/shopping_list.py
@@ -1,3 +1,5 @@
+# Creates a numbered shopping list using enumerate().
+
 shopping_list = ["Milk", "Bread" , "Eggs","Biscuits"]
 for index, item in enumerate(shopping_list, start=1):
     print(f"{index}. {item}")
