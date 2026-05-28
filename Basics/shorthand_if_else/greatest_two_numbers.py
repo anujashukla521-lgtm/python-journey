9d9405d1eadcf576d53007a6eeada883f69252bb
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-28 17:17:49 +0530

    Code 5

--- Basics/shorthand_if_else/greatest_two_numbers.py
+++ Basics/shorthand_if_else/greatest_two_numbers.py
@@ -1,3 +1,5 @@
+# Finds the greater number between two inputs using shorthand if-else.
+
 num1 = int(input("Enter first num: "))
 num2 = int(input("Enter second num: "))
 print(f"{num1} is greater") if num1>num2 else print("Both are equal") if num1==num2 else print(f"{num2} is greater")
