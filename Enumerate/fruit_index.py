a218b3ecf184466383839c2ab5fc9fd366cce823
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-28 17:55:13 +0530

    Code 1

--- Enumerate/fruit_index.py
+++ Enumerate/fruit_index.py
@@ -1,3 +1,5 @@
+# Prints fruits along with their index positions.
+
 fruits = ["Apple" , "Mango" , "Banana" , "Strawberry"]
 for index, fruit_name in enumerate(fruits):
     print(index, fruit_name)
