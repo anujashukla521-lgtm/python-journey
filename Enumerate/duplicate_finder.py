058291e695acf926c6f3f4683a3b4056e4816791
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-28 17:57:46 +0530

    Code 6

--- Enumerate/duplicate_finder.py
+++ Enumerate/duplicate_finder.py
@@ -1,7 +1,7 @@
 nums = [7,9,4,7,2,5,1]
-duplicate = []
+seen_nums = []
 for index, numbers in enumerate(nums):
-    if numbers in duplicate:
+    if numbers in seen_nums:
         print(f"Duplicate: {numbers} at index {index}")
     else:
-        duplicate.append(numbers)
+        seen_nums.append(numbers)
