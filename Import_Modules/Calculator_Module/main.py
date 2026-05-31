8dbda1c08b1684a3f545075654b7b8fb99fc3c94
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-05-31 12:17:25 +0530

    Code 1

---
+++ Import_Modules/Calculator_Module/main.py
@@ -0,0 +1,10 @@
+import calculator as calc
+
+x = int(input("Enter first num: "))
+y = int(input("Enter second num: "))
+
+result1 = calc.add(x,y)
+result2 = calc.subtract(x,y)
+
+print("Addition", result1)
+print("Subtraction",result2)
