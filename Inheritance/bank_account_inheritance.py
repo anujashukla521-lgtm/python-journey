a0920fadba40d49c4bbee416ff0dde79fc0c5440
ANUJA SHUKLA <anujashukla521@gmail.com>
2026-06-18 12:09:16 +0530

    Add files via upload

---
+++ Inheritance/bank_account_inheritance.py
@@ -0,0 +1,43 @@
+class BankAccount:
+    def __init__(self,account_holder,account_type,balance):
+        self.account_holder = account_holder
+        self._account_type = account_type
+        self.__balance = balance
+
+    def deposit(self,amount):
+        if amount>0:
+            self.__balance+=amount
+        else:
+            return "Invalid amount"
+
+        return "Deposited successfully"
+
+    def withdraw(self,amount):
+        if amount>0 and amount<=self.__balance:
+            self.__balance-=amount
+        else:
+            return "No amount available"
+
+        return "Withdrawal successfully"
+
+    def display_details(self):
+        print("\n====DISPLAYING INFO====")
+        print(f"Account holder: {self.account_holder}")
+        print(f"Account type: {self._account_type}")
+        print(f"Balance: {self.__balance}")
+
+try:
+    holder = input("Enter account holder name: ")
+    acc_type = input("Enter account type: ")
+    balance = float(input("Enter balance: "))
+    bank1 = BankAccount(holder,acc_type,balance)
+    bank1.display_details()
+    balance_D = float(input("Enter balance to deposit: "))
+    print(bank1.deposit(balance_D))
+    bank1.display_details()
+    balance_W = float(input("Enter balance to withdraw: "))
+    print(bank1.withdraw(balance_W))
+    bank1.display_details()
+
+except ValueError as e:
+    print(e)
